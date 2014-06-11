176974f99ce43c9cbbdc61c711e36696e2e1f290
yoyo83yoyo <101703031@nccu.edu.tw>
2014-06-11 21:53:20 +0800

    Update search.js

--- search.js
+++ search.js
@@ -1,25 +1,17 @@
 function messageGo(){
-  //���o "username" ������
-  var username = $('#search').val();        
-   //���o "message" ������        
+  
+  var username = $('#type').val();        
   var message = $('#days').val();             
     $.ajax({
-        //�i�D�{�������n�ǰe������ 
-        url:"",
-        //�ݭn�ǰe������
-data:"&message="+message+"&username="+username, 
-         //�ϥ�POST���k     
-        type : "POST",         
-        //�����^�Ǹ��ƪ��榡�A�b�o�ӨҤl���A�u�n�O����true�N�i�H�F
+        url:"",        
+        data:"&message="+message+"&username="+username, 
+        
+                type : "POST",         
         dataType:'json', 
-         //�ǰe���ѫh���X���ѰT��      
         error:function(){ 
-        //���ƶǰe���ѫ��N�|�����o��function�����{���A�i�H�b�o�̼g�J�n���檺�{��  
         alert("132");
         },
-        //�ǰe���\�h���X���\�T��
-        success:function(){
-        //���ƶǰe���\���N�|�����o��function�����{���A�i�H�b�o�̼g�J�n���檺�{��  
+        success:function()
         alert("123123");
         }
     }); 
