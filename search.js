32c4918d3eb5f1b66ad78ea4806cfe11f4d9c36f
yoyo83yoyo <101703031@nccu.edu.tw>
2014-06-11 17:28:20 +0800

    Update search.js

--- search.js
+++ search.js
@@ -24,4 +24,22 @@ data:"&message="+message+"&username="+username,
         }
     }); 
 }
-
+function send(){
+ var query = $('#query').valueOf();
+    $.ajax({
+        url: '/yoyo83yoyo/search',
+        type: "POST",
+        data: query,
+        dataType: 'application/json; charset=utf-8',
+        success: function (data) {
+            alert(data);
+            for (var x = 0; x < data.length; x++) {
+                content = data[x].Id;
+                content += "<br>";
+                content += data[x].Name;
+                content += "<br>";
+                $(content).appendTo("#ProductList");
+            }
+        }
+    });
+});
