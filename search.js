64f6fc0df74796de9994fd7b051dafd9321cc9ff
yoyo83yoyo <101703031@nccu.edu.tw>
2014-06-12 10:20:37 +0800

    Update search.js

--- search.js
+++ search.js
@@ -44,3 +44,13 @@ function messageGo(){
     });
 });
 */
+$("button").click(function(){
+  $.post("demo_test_post.asp",
+  {
+    name:"Donald Duck",
+    city:"Duckburg"
+  },
+  function(data,status){
+    alert("Data: " + data + "\nStatus: " + status);
+  });
+});
